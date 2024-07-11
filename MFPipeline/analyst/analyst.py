f0d15a956fee154286ac5f0e807697a7147df912
Katarzyna Kruszynska <kruszynskakat@gmail.com>
2024-07-11 15:12:07 -0700

    Created Analyst class.

---
+++ MFPipeline/analyst/analyst.py
@@ -0,0 +1,55 @@
+import yaml
+
+class Analyst:
+    '''
+    This is a class that analyzes one event.
+    This class creates bare bones of other analysts and contains elements they all share.
+
+    :param event_name: str, name of the analyzed event
+    :param ra: float, Right Ascention of the analyzed event
+    :param dec: float, declination of the analyzed event
+    :param analyst_path: str, path to the folder where the outputs are saved
+    :param config_dict: dictionary, optional, dictionary with Analyst configuration
+    :param config_path: str, optional, path to the YAML configuration file of the Analyst
+    '''
+    def __init__(self,
+                 event_name,
+                 analyst_path,
+                 config_dict=None,
+                 config_path=None):
+
+        self.event_name = event_name
+        self.analyst_path = analyst_path
+
+        self.config = {}
+        if (config_dict != None):
+            # READ config_dict
+            self.config = config_dict
+        elif (config_path != None):
+            # read config path
+            self.parse_config(config_path)
+        else:
+            # todo: raise custom exception here?
+            print("Error! Analyst needs information!!!")
+            quit()
+
+    def parse_config(self, config_path):
+        '''
+        Parse YAML file with configuration, turn it into a dictionary and add it to self.
+
+        :param config_path: str, path with YAML file containing Analyst configuration.
+
+        '''
+
+        config = {}
+        try:
+            with open(config_path, 'r') as file:
+                event_config = yaml.safe_load(file)
+
+            self.config["event_name"] = event_config.get("event_name")
+            self.config["ra"] = event_config.get("ra")
+            self.config["dec"] = event_config.get("dec")
+
+        except Exception as err:
+            print(f"Unexpected %s, %s" % (err, type(err)))
+
