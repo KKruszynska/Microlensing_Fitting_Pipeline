028825095906afc96a54eb680b78af1919ebde34
Katarzyna Kruszynska <kruszynskakat@gmail.com>
2024-07-11 15:11:13 -0700

    Small changes.

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -7,8 +7,7 @@ class TestController:
         from MFPipeline.controller.controller import Controller
 
         event_list = ["GDR3-ULENS-025", "GDR3-ULENS-025_copy"]#"GDR3-ULEN-118"]
-        # / home / katarzyna / Documents / Microlensing_Fitting_Pipeline / Microlensing_Fitting_Pipeline / tests / test_controller / GDR3 - ULENS - 025 / config.yaml
-        config = {"events_path": "/home/katarzyna/Documents/Microlensing_Fitting_Pipeline/Microlensing_Fitting_Pipeline/tests/test_controller"}
+        config = {"events_path": "tests/test_controller"}
         controller = Controller(event_list, config_dict=config)
         controller.create_analysts()
         controller.launch_analysts()
