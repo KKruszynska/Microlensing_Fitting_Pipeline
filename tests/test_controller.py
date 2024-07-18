08e9f0eef11b36676e7365aa4ddde5f37222c7f3
Katarzyna Kruszynska <kruszynskakat@gmail.com>
2024-07-18 16:23:19 -0700

    Updated tests to run subprocesses.

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,4 +1,10 @@
-class TestController:
+import glob, os
+import numpy as np
+import pandas as pd
+
+import json
+
+class TestControllerPaths:
     '''
     Tests to check if controller works fine.
     '''
@@ -6,8 +12,95 @@ class TestController:
     def test_launch_analysts(self):
         from MFPipeline.controller.controller import Controller
 
-        event_list = ["GDR3-ULENS-025", "GDR3-ULENS-025_copy"]#"GDR3-ULEN-118"]
-        config = {"events_path": "tests/test_controller"}
+        event_list = ["GaiaDR3-ULENS-018",
+                      "GaiaDR3-ULENS-025"]
+        config = {
+            "events_path": "/home/katarzyna/Documents/Microlensing_Fitting_Pipeline/Microlensing_Fitting_Pipeline/tests/test_controller/",
+            "python_compiler": "python",
+            "software_dir": "/home/katarzyna/Documents/Microlensing_Fitting_Pipeline/Microlensing_Fitting_Pipeline/MFPipeline/analyst/",
+            "group_processing_limit": 3,
+            }
+
         controller = Controller(event_list, config_dict=config)
-        controller.create_analysts()
+        controller.launch_analysts()
+
+class TestControllerDicts:
+    '''
+    Tests to check if controller works fine.
+    '''
+
+    def test_launch_analysts(self):
+        from MFPipeline.controller.controller import Controller
+
+
+
+        event_list = [#"GaiaDR3-ULENS-018",
+                      "GaiaDR3-ULENS-025"]
+
+        event_info = pd.read_csv("tests/test_controller/events_info.csv", header=0)
+
+        analyst_jsons = {}
+        path_lightcurves = "tests/test_controller/light_curves/"
+        os.chdir(path_lightcurves)
+        for event in event_list:
+            dictionary = {}
+            idx = event_info.index[event_info["#event_name"] == event].tolist()
+            dictionary["event_name"] = event
+            dictionary["ra"], dictionary["dec"] = "%f"%event_info["ra"].values[idx][0], "%f"%event_info["dec"].values[idx][0]
+            dictionary["lc_analyst"] = {}
+            dictionary["lc_analyst"]["n_max"] = "%d"%event_info["lc_nmax"].values[idx][0]
+            dictionary["fit_analyst"] = {}
+            dictionary["fit_analyst"]["n_max"] = "%d"%event_info["fit_nmax"].values[idx][0]
+
+            cats = event_info["catalogues"].values[idx][0].split(" ")
+            cat_list = []
+            for catalogue in cats:
+                dict = {}
+                dict["name"] = catalogue
+                dict["bands"] = event_info["bands"].values[idx][0].split(" ")
+                if (len(event_info["path"].values[idx]) > 0):
+                    dict["cmd_path"] = event_info["path"].values[idx][0]
+                    dict["cmd_separator"] = ","
+                cat_list.append(dict)
+            dictionary["cmd_analyst"] = {}
+            dictionary["cmd_analyst"]["catalogues"] = cat_list
+
+            light_curves = []
+            for file in glob.glob("*%s*.dat" % event):
+                light_curve = np.genfromtxt(file, usecols=(0, 1, 2), unpack=True)
+                light_curve = light_curve.T
+
+                survey = ""
+                band = ""
+                if ("mod" in file):
+                    survey = "Gaia"
+                    txt = file.split(".")
+                    band = txt[0].split("_")[-1]
+                    light_curve[:, 0] = light_curve[:, 0] + 2450000.
+                elif ("OGLE" in file):
+                    survey = "OGLE"
+                    band = "I"
+                elif ("KMT" in file):
+                    survey = "KMTNet_" + file[-7]
+                    band = "I"
+
+                dict = {}
+                dict["survey"] = survey
+                dict["band"] = band
+                dict["lc"] = json.dumps(light_curve.tolist())
+                light_curves.append(dict)
+
+            dictionary["light_curves"] = light_curves
+            js = json.dumps(dictionary)
+            analyst_jsons[event] = js
+
+        os.chdir("../../../")
+        config = {
+            "events_path": "/tests/test_controller/",
+            "python_compiler": "python",
+            "software_dir": "MFPipeline/analyst/",
+            "group_processing_limit": 3,
+            }
+
+        controller = Controller(event_list, config_dict=config, analyst_dicts=analyst_jsons)
         controller.launch_analysts()
