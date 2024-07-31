20dcb4728ca4d31a2641658b05aebd2d4ea0f510
Katarzyna Kruszynska <kruszynskakat@gmail.com>
2024-07-31 11:07:32 -0700

    Added log statements for LC Analyst.

--- MFPipeline/analyst/light_curve_analyst.py
+++ MFPipeline/analyst/light_curve_analyst.py
@@ -13,6 +13,8 @@ class LightCurveAnalyst(Analyst):
 
     :param event_name: str, name of the analyzed event
     :param analyst_path: str, path to the folder where the outputs are saved
+    :param light_curves: dict, dictionary containing light curves, observatory name, and filter
+    :param log: logger instance, log started by Event Analyst
     :param config_dict: dictionary, optional, dictionary with Event Analyst configuration
     :param config_path: str, optional, path to the YAML configuration file of the Event Analyst
     '''
@@ -20,6 +22,7 @@ class LightCurveAnalyst(Analyst):
                  event_name,
                  analyst_path,
                  light_curves,
+                 log,
                  config_dict=None,
                  config_path=None):
 
@@ -27,13 +30,14 @@ class LightCurveAnalyst(Analyst):
         # Analyst.__init__(self, event_name, analyst_path, config_dict=config_dict, config_path=config_path)
 
         self.light_curves = light_curves
+        self.log = log
 
         if (config_dict != None):
             self.add_lc_config(config_dict)
         elif("lc_analyst" in self.config):
             self.add_lc_config(self.config)
         else:
-            print("Error! Light Curve Analyst needs information.")
+            self.log.error("LC Analyst: Error! Light Curve Analyst needs information.")
             quit()
 
 
@@ -44,19 +48,28 @@ class LightCurveAnalyst(Analyst):
         :param config_dict: dict, dictionary with analyst config
         '''
 
+        self.log.debug("LC Analyst: Reading lc config.")
         self.n_max = int(config["lc_analyst"]["n_max"])
+        self.log.debug("LC Analyst: Finished reading lc config.")
 
     def perform_quality_check(self):
         '''
-        Placeholder. Counts to n_max.
+        Placeholder. Wait `self.n_max` seconds.
 
-        :return: number of counts
+        :return: Status of the operation
         '''
 
+        status = False
+
+        self.log.info("LC Analyst: Start quality check.")
         for lc in self.light_curves:
             #extract np array with the light curve
             # null_entries = self.flag_NULL_entries(lc)
-            sleep(5)
+            sleep(self.n_max)
+            status = True
+        self.log.info("LC Analyst: Quality check ended.")
+
+        return status
 
 
     def flag_NULL_entries(light_curve):
@@ -64,4 +77,10 @@ class LightCurveAnalyst(Analyst):
         #find all NaNs using isnull() function
         mask_null = np.where(light_curve[:,1] == "NULL")
 
+    def flag_huge_errorbars(light_curve):
+        '''
 
+        :return:
+        '''
+
+        return 0.
