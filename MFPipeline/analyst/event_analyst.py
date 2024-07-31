0ee16058b96b691753e3a0e388d479cb8c89513d
Katarzyna Kruszynska <kruszynskakat@gmail.com>
2024-07-31 11:03:52 -0700

    Added log statements for Event Analyst.

--- MFPipeline/analyst/event_analyst.py
+++ MFPipeline/analyst/event_analyst.py
@@ -9,6 +9,8 @@ from MFPipeline.analyst.light_curve_analyst import LightCurveAnalyst
 from MFPipeline.analyst.fit_analyst import FitAnalyst
 from MFPipeline.analyst.cmd_analyst import CmdAnalyst
 
+from MFPipeline import logs
+
 
 class EventAnalyst(Analyst):
     '''
@@ -21,25 +23,37 @@ class EventAnalyst(Analyst):
 
     :param event_name: str, name of the analyzed event
     :param analyst_path: str, path to the folder where the outputs are saved
+    :param log_level: str, level of logging
     :param config_dict: dictionary, optional, dictionary with Event Analyst configuration
     :param config_path: str, optional, path to the YAML configuration file of the Event Analyst
+    :param stream: optional, boolean, should the log be accessible through Kubernetes?
     '''
+
     def __init__(self,
                  event_name,
                  analyst_path,
+                 log_level,
                  config_dict=None,
-                 config_path=None):
+                 config_path=None,
+                 stream=False,
+                 ):
 
         super().__init__(event_name, analyst_path, config_dict=config_dict, config_path=config_path)
         # Analyst.__init__(self, event_name, analyst_path, config_dict=config_dict, config_path=config_path)
 
-        if(config_path != None):
+        # start
+        self.log = logs.start_analyst_log(event_name, analyst_path, log_level, stream=stream)
+        self.log.info("-------------------------------------------")
+        self.log.info("Event Analyst: Analyzing event %s"%(event_name))
+        self.log.info("-------------------------------------------")
+
+        if (config_path != None):
             self.parse_config(config_path)
             self.parse_event_config(config_path)
-        elif(config_dict != None):
+        elif (config_dict != None):
             self.add_config_dict(config_dict)
         else:
-            print("Error! Event Analyst needs information.")
+            self.log.error("Event Analyst: Error! Event Analyst needs information.")
             quit()
 
     def parse_event_config(self, config_path):
@@ -57,10 +71,10 @@ class EventAnalyst(Analyst):
             self.config["fit_analyst"] = event_config.get("fit_analyst")
             self.config["cmd_analyst"] = event_config.get("cmd_analyst")
 
-            self.light_curves  = self.parse_light_curves(event_config.get("light_curves"))
+            self.light_curves = self.parse_light_curves(event_config.get("light_curves"))
 
         except Exception as err:
-            print(f"Unexpected %s, %s" % (err, type(err)))
+            self.log.error(f"Event Analyst: %s, %s" % (err, type(err)))
 
     def add_config_dict(self, conifg_dict):
         '''
@@ -78,7 +92,7 @@ class EventAnalyst(Analyst):
 
 
         except Exception as err:
-            print(f"Unexpected %s, %s" % (err, type(err)))
+            self.log.error(f"Event Analyst: %s, %s" % (err, type(err)))
 
     def parse_light_curves(self, lc_config):
         '''
@@ -97,7 +111,7 @@ class EventAnalyst(Analyst):
                 light_curve = entry["lc"]
                 light_curves.append((self.event_name, light_curve, survey, band))
             else:
-                print("Problem! No light curve data specified")
+                self.log.error("Event Analyst: Problem! No light curve data specified")
 
         return light_curves
 
@@ -110,46 +124,60 @@ class EventAnalyst(Analyst):
         :return: status?
         '''
 
+        self.log.info("Event Analyst: Processing started.")
+
         lc_status = self.run_lc_analyst()
 
         fit_status = self.run_fit_analyst()
 
         cmd_plot_status = self.run_cmd_analyst()
-        if False in cmd_plot_status:
-            # this should be a log statement, and more informative
-            print("Error encountered while creating CMD plots.")
+
+        self.log.info("Event Analyst: Processing finished.")
+        self.log.info("-------------------------------------------")
+        logs.close_log(self.log)
 
     def run_lc_analyst(self):
         '''
         Launch Light Curve Analyst to check the quality of the light curve.
         Placeholder.
-
-        :return: not sure yet?.
         '''
-        # todo: print -> log statements.
 
-        lc_quality_status = 0.
-        lc_analyst = LightCurveAnalyst(self.config["event_name"], self.analyst_path, self.light_curves, config_dict=self.config)
+        lc_quality_status = False
+
+        self.log.info("Event Analyst: Starting Light Curve Analyst.")
+        lc_analyst = LightCurveAnalyst(self.config["event_name"], self.analyst_path, self.light_curves,
+                                       self.log,
+                                       config_dict=self.config
+                                       )
+        self.log.debug("Event Analyst: Light Curve Analyst Created.")
+        self.log.debug("Event Analyst: Starting Light Curve quality check.")
         lc_quality_status = lc_analyst.perform_quality_check()
+        self.log.debug("Event Analyst: Light Curve quality check ended.")
 
-        return lc_quality_status
+        if (lc_quality_status):
+            self.log.info("Event Analyst: Lc quality procedures finished succesfully.")
+        else:
+            self.log.info("Event Analyst: LC Analyst finished with errors.")
 
     def run_fit_analyst(self):
         '''
         Launch Fit Analyst to find all fitting microlensing events.
         Placeholder.
-
-        :return: not sure yet?.
         '''
-        # todo: print -> log statements.
 
-        fit_status = 0.
+        fit_status = False
+        self.log.info("Event Analyst: Starting Fit Analyst.")
         fit_analyst = FitAnalyst(self.config["event_name"], self.analyst_path, self.light_curves,
-                                       config_dict=self.config
-                                       )
+                                 config_dict=self.config
+                                 )
+        self.log.debug("Event Analyst: Fit Analyst created.")
+        self.log.debug("Event Analyst: Starting fitting.")
         fit_status = fit_analyst.perform_fit()
-
-        return fit_status
+        self.log.debug("Event Analyst: Fitting finished.")
+        if (fit_status):
+            self.log.info("Event Analyst: Fitting finished succesfully.")
+        else:
+            self.log.info("Event Analyst: Fit Analyst finished with errors.")
 
     def run_cmd_analyst(self):
         '''
@@ -157,40 +185,91 @@ class EventAnalyst(Analyst):
 
         :return: a list of boolean values corresponding to satuts of the created cmd plots.
         '''
-        # todo: probably better log if CMD was succesful instead of returning a list with status???
         cmd_plot_status = []
 
         for dictionary in self.config["cmd_analyst"]["catalogues"]:
             catalogue = dictionary["name"]
-            light_curve_data = {'baseline': {'Gaia_G': 16.12, 'Gaia_BP': 17.78, 'Gaia_RP': 14.88}, 'source': {'Gaia_G': 16.14, 'Gaia_BP': 17.79, 'Gaia_RP': 14.91}, 'blend': {'Gaia_G': 20.37, 'Gaia_BP': 22.78, 'Gaia_RP': 18.69}}
+            self.log.info("Event Analyst: Starting CMD analyst for %s" % catalogue)
+            light_curve_data = {'baseline': {'Gaia_G': 16.12, 'Gaia_BP': 17.78, 'Gaia_RP': 14.88},
+                                'source': {'Gaia_G': 16.14, 'Gaia_BP': 17.79, 'Gaia_RP': 14.91},
+                                'blend': {'Gaia_G': 20.37, 'Gaia_BP': 22.78, 'Gaia_RP': 18.69}}
+            self.log.debug("Event Analyst: Got light curve data.")
 
-            cmd_analyst = CmdAnalyst(self.config["event_name"], self.analyst_path, catalogue, light_curve_data, config_dict=self.config)
+            cmd_analyst = CmdAnalyst(self.config["event_name"], self.analyst_path, catalogue, light_curve_data,
+                                     config_dict=self.config
+                                     )
+            self.log.debug("Event Analyst: CMD Analyst created.")
 
             source_data, source_labels = cmd_analyst.transform_source_data()
+            self.log.debug("Event Analyst: Finished transforming source data.")
+
             cmd_data, cmd_labels = cmd_analyst.load_catalogue_data()
+            self.log.debug("Event Analyst: Finished loading catalogue data.")
 
             plot_status = cmd_analyst.plot_cmd(source_data, source_labels, cmd_data, cmd_labels)
-            cmd_plot_status.append(plot_status)
+            self.log.debug("Event Analyst: finished creating plot.")
+            if (plot_status):
+                self.log.info("Event Analyst: CMD plot created successfully for %s."%catalogue)
+            else:
+                self.log.error("Event Analyst: Problems while creating CMD plot for %s." % catalogue)
 
-        return cmd_plot_status
 
 if __name__ == "__main__":
+    log_level = ""
+    stream = False
+    event = ""
+    analyst_path = ""
+    error = False
+    error_string = ""
+
+    if ("--event_name"):
+        idx = sys.argv.index("--event_name")
+        event += sys.argv[idx + 1]
+    else:
+        error = True
+        error_string += "Event Analyst: Error! Missing event name!\n"
+
+    if ("--analyst_path"):
+        idx = sys.argv.index("--analyst_path")
+        analyst_path += sys.argv[idx + 1]
+    else:
+        error = True
+        error_string += "Event Analyst: Error! Missing analyst path!\n"
+
+    if ("--log_level"):
+        idx = sys.argv.index("--log_level")
+        log_level += sys.argv[idx + 1]
+    else:
+        error = True
+        error_string += "Event Analyst: Error! Missing log level information!\n"
+
+    if ("--stream"):
+        idx = sys.argv.index("--stream")
+        stream = bool(sys.argv[idx + 1])
+
     if ("--config_path" in sys.argv):
-        idx = sys.argv.index("--event_name")
-        event = sys.argv[idx + 1]
-        idx = sys.argv.index("--analyst_path")
-        analyst_path = sys.argv[idx+1]
-        event_analyst = EventAnalyst(event, analyst_path, config_path=analyst_path + "config.yaml")
-        event_analyst.run_single_analyst()
+        event_analyst = EventAnalyst(event, analyst_path, log_level,
+                                     config_path=analyst_path + "config.yaml",
+                                     stream=stream
+                                     )
     elif ("--config_dict" in sys.argv):
-        idx = sys.argv.index("--event_name")
-        event = sys.argv[idx + 1]
-        idx = sys.argv.index("--analyst_path")
-        analyst_path = sys.argv[idx + 1]
         idx = sys.argv.index("--config_dict")
         config = json.loads(sys.argv[idx + 1])
-        event_analyst = EventAnalyst(event, analyst_path, config_dict=config)
-        event_analyst.run_single_analyst()
-        print(event, ": Finished!")
+        event_analyst = EventAnalyst(event, analyst_path, log_level,
+                                     config_dict=config
+                                     )
     else:
-        print("Error encountered while running an Event Analyst.")
+        error = True
+        error_string += "Event Analyst: Error! No config specified!"
+
+    if (error):
+        if ((len(log_level) > 0) and (len(analyst_path) > 0)):
+            log = logs.start_analyst_log(event_name, analyst_path, log_level, stream=stream)
+            log.error("Event Analyst: Error encountered while running an Event Analyst.\n")
+            log.error(error_string)
+            logs.close_log(log)
+        else:
+            print("Event Analyst: Error encountered while running an Event Analyst.\n")
+            print(error_string)
+    else:
+        event_analyst.run_single_analyst()
