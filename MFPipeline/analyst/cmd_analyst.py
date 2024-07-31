8a668b2f4f01e049916db37da4508dea9cfd40db
Katarzyna Kruszynska <kruszynskakat@gmail.com>
2024-07-31 11:03:39 -0700

    Added log statements for CMD Analyst.

--- MFPipeline/analyst/cmd_analyst.py
+++ MFPipeline/analyst/cmd_analyst.py
@@ -18,7 +18,11 @@ class CmdAnalyst(Analyst):
 
     :param event_name: str, name of the event
     :param analyst_path: str, path to the folder where the outputs are saved
+    :param catalogue: str, name of the catalogue
     :param light_curve_data: dict, dictionary with magnitudes in selected bands for source, blend and baseline; this is a result from fitting.
+    :param log: logger instance, log started by Event Analyst
+    :param config_dict: dictionary, optional, dictionary with CMD Analyst configuration
+    :param config_path: str, optional, path to the YAML configuration file of the CMD Analyst
 
     Notes on configuration:
 
@@ -39,22 +43,24 @@ class CmdAnalyst(Analyst):
                  analyst_path,
                  catalogue,
                  light_curve_data,
+                 log,
                  config_dict=None,
                  config_path=None):
 
         Analyst.__init__(self, event_name, analyst_path, config_dict=config_dict, config_path=config_path)
+        self.log = log
 
         if(light_curve_data != None):
             self.light_curve_data = light_curve_data
         else:
-            print("Error! CMD Analyst needs source and blend magnitudes.")
+            self.log.error("CMD Analyst: Error! CMD Analyst needs source and blend magnitudes.")
 
         if (config_dict != None):
             self.add_cmd_config(catalogue, config_dict)
         elif ("cmd_analyst" in self.config):
             self.add_cmd_config(catalogue, self.config)
         else:
-            print("Error! CMD Analyst needs information.")
+            self.log.error("CMD Analyst: Error! CMD Analyst needs information.")
             quit()
 
     def add_cmd_config(self, catalogue, config_dict):
@@ -65,12 +71,17 @@ class CmdAnalyst(Analyst):
         :param config_dict: dict, dictionary with analyst config
         '''
 
+        self.log.debug("CMD Analyst: Adding config.")
         cmd_config = \
             [cat_dict for cat_dict in config_dict["cmd_analyst"]["catalogues"] if cat_dict["name"] == catalogue][0]
         self.catalogue_name = cmd_config.get("name")
         self.radius = cmd_config.get("radius", 30. / 60.)
         self.file_path = cmd_config.get("cmd_path", None)
         self.optional_kwargs = cmd_config.get("optional", None)
+        self.log.debug("CMD Analyst: Finished adding config.")
+        self.log.debug("CMD Analyst: Added fields: %s,\n %s,\n %s,\n %s"%(self.catalogue_name, self.radius,
+                                                        self.file_path, self.optional_kwargs))
+
     def transform_source_data(self):
         '''
         This function transforms a dictionary (`light_curve_data`) with source, blend and baseline information into two
@@ -80,6 +91,7 @@ class CmdAnalyst(Analyst):
         :return: two data structures, a pandas data frame and band labels.
         '''
         try:
+            self.log.debug("CMD Analyst: Creating source and blend mags dataframe and labels.")
             d = []
             l = []
             for key in self.light_curve_data:
@@ -88,15 +100,17 @@ class CmdAnalyst(Analyst):
                     row.append(self.light_curve_data[key][inner_key])
                     l.append(inner_key)
                 d.append(row)
+            self.log.debug("CMD Analyst: Collected all labels.")
 
             labels = l[:len(d[0][:]) - 1]
             cols = ['object']
             for l in labels:
                 cols.append(l)
             data = pd.DataFrame(d, columns=cols)
+            self.log.debug("CMD Analyst: Source and blend mags dataframe created.")
 
         except Exception as err:
-            print(f"Unexpected %s, %s" % (err, type(err)))
+            self.log.error(f"CMD Analyst: %s, %s" % (err, type(err)))
             data = None
             labels = None
 
@@ -110,16 +124,24 @@ class CmdAnalyst(Analyst):
         :return: a pandas data frame with data to create a cmd, and a list with band labels
         '''
 
+        self.log.debug("CMD Analyst: Preparing to load the catalogue.")
         if self.file_path is not None:
             if (self.optional_kwargs != None and "separator" in self.optional_kwargs):
+                self.log.debug("CMD Analyst: Loading catalogue from file with specified separator.")
                 data, labels = self.load_catalogue_from_file_data(separator=self.optional_kwargs["separator"])
             else:
+                self.log.debug("CMD Analyst: Loading catalogue from file with default (,) separator.")
                 data, labels = self.load_catalogue_from_file_data()
         else:
+            self.log.debug("CMD Analyst: Loading catalogue from online catalogues.")
             if "Gaia" in self.catalogue_name:
                 if (self.optional_kwargs != None and "parallax_quality" in self.optional_kwargs):
+                    self.log.debug("CMD Analyst: Loading catalogue from online catalogue with parallax_quality=%d."%
+                                   int(self.optional_kwargs["parallax_quality"]))
                     data, labels = self.load_gaia_data(parallax_quality=self.optional_kwargs["parallax_quality"])
                 else:
+                    self.log.debug("CMD Analyst: Loading catalogue from online catalogue with \
+                                   default parallax_quality=5.")
                     data, labels = self.load_gaia_data()
 
 
@@ -143,6 +165,8 @@ class CmdAnalyst(Analyst):
         if "DR1" in self.catalogue_name:
             table_name = "gaiadr1"
 
+        self.log.debug("CMD Analyst: Gaia catalogue chosen for %s: %s."%(self.catalogue_name, table_name))
+
         try:
             if "DR3" in self.catalogue_name:
                 adql_query = ("SELECT source_id, phot_g_mean_mag, phot_bp_mean_mag, phot_rp_mean_mag \
@@ -158,9 +182,12 @@ class CmdAnalyst(Analyst):
                              CONTAINS(POINT(ra, dec), CIRCLE(%f, %f, %f))=1;"%
                               (table_name, int(parallax_quality), self.config["ra"], self.config["dec"], self.radius))
 
+            self.log.debug("CMD Analyst: Querying Gaia.")
+            self.log.debug("CMD Analyst: Query:\n %s"%adql_query)
             Gaia.load_tables(only_names=True)
             job = Gaia.launch_job_async(adql_query)
             result = job.get_results()
+            self.log.debug("CMD Analyst: Query response retrieved.")
 
             data = {"Gaia_G" : result["phot_g_mean_mag"],
                     "Gaia_BP" : result["phot_bp_mean_mag"],
@@ -168,9 +195,10 @@ class CmdAnalyst(Analyst):
                     }
             data_frame = pd.DataFrame(data=data)
             labels = ["Gaia_G", "Gaia_BP", "Gaia_RP"]
+            self.log.debug("CMD Analyst: Response reformatted to dataframe, labels created.")
 
         except Exception as err:
-            print(f"Unexpected %s, %s" % (err, type(err)))
+            self.log.error(f"CMD Analyst: %s, %s" % (err, type(err)))
             data_frame = None
             labels = None
 
@@ -183,8 +211,16 @@ class CmdAnalyst(Analyst):
         :return: pandas data frame and labels of the bands
         '''
 
-        data = pd.read_csv(self.file_path, sep=separator)
-        labels = data.columns.tolist()
+        try:
+            self.log.debug("CMD Analyst: Attempting to read the data from file.")
+            data = pd.read_csv(self.file_path, sep=separator)
+            labels = data.columns.tolist()
+            self.log.debug("CMD Analyst: Data loaded to dataframe, labels created.")
+
+        except Exception as err:
+            self.log.error(f"CMD Analyst: %s, %s" % (err, type(err)))
+            data = None
+            labels = None
 
         return data, labels
 
@@ -201,6 +237,7 @@ class CmdAnalyst(Analyst):
         :return: status of creating a cmd plot
         '''
 
+        self.log.debug("CMD Analyst: Plotting CMD started.")
         for i in range(len(cmd_labels)):
 
             colours = {"baseline": "#000000",  # black
@@ -224,6 +261,7 @@ class CmdAnalyst(Analyst):
                                          name=self.catalogue_name,
                                          showlegend=True
                                          ), )
+                self.log.debug("CMD Analyst: Catalogue data plotted.")
 
                 for j in range(len(source_data['object'].values[:])):
                     fig.add_trace(
@@ -237,6 +275,7 @@ class CmdAnalyst(Analyst):
                                    name=source_data['object'].iloc[j],
                                    showlegend=True
                                    ), )
+                    self.log.debug("CMD Analyst: Plotted data for %s."%source_data['object'].iloc[j])
 
                 fig.update_layout(
                     title="%s CMD" % self.event_name,
@@ -255,20 +294,23 @@ class CmdAnalyst(Analyst):
                     paper_bgcolor="rgba(0,0,0,0)",
                     plot_bgcolor="rgba(0,0,0,0)"
                 )
+                self.log.debug("CMD Analyst: Title, legend and axes titles created.")
 
                 fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor="lightgray")
                 fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor="lightgray")
                 fig.update_xaxes(zeroline=True, zerolinewidth=1, zerolinecolor="lightgray")
+                self.log.debug("CMD Analyst: Grid added.")
 
                 os.makedirs("./"+self.analyst_path, exist_ok=True)
                 fig.write_html(
                     "%s/%s_CMD_%s_%s.html" % ("./"+self.analyst_path, self.event_name, self.catalogue_name, cmd_labels[i])
                     )
+                self.log.debug("CMD Analyst: Plot saved.")
 
                 cmd_status = True
 
             except Exception as err:
-                print(f"Unexpected %s, %s" % (err, type(err)))
+                self.log.error(f"CMD Analyst: %s, %s" % (err, type(err)))
                 cmd_status = False
 
         return cmd_status
